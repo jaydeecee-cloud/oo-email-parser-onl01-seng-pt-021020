e59018397b03ac9da174acf071d73f75e203e094
Joseph Choi <jdc07@live.com>
2020-04-14 15:39:39 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,18 +9,4 @@ class EmailParser
 
   def initialize(emails)
     @emails = emails
-  end
-
-  def parse
-    email_array = []
-    email_array = @emails.split(/[,\s]/)
-    email_array = email_array.reject { |e| e == ""}
-    email_array.uniq
-  end
-
-end
-
-emails = "hpotter@hogwarts.org, he_who_must_not_be_named@riddleshouse.net"
-parser = EmailParser.new(emails)
-
-puts parser.parse
+  
